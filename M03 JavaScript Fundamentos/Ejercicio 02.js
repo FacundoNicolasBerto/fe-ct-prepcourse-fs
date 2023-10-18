25b4b25d19316f64fe95340d7322ae22e959e56e
FacundoNicolasBerto <facundonberto@gmail.com>
2023-10-18 03:04:43 -0300

    Retomar desde Ejercicio04

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var operacion = x + y;
+   return operacion;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var operacion2 = x - y ;
+   return operacion2;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var operacion3 = x / y ;
+   return operacion3;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var operacion4 = x * y;
+   return operacion4;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var operacion5 = x % y;
+   return operacion5;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
